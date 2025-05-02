cdfbf6af0ada87fd7200d25b9fc37c324adb2e80
Alan <63990513+athekat@users.noreply.github.com>
2025-05-02 19:12:01 -0300

    Update settings.py

--- aoewebapp/settings.py
+++ aoewebapp/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
