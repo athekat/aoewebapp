1113fd1231b96ed8df0c3752d47a51c4f7bf3d26
Alan <63990513+athekat@users.noreply.github.com>
2025-05-02 18:26:59 -0300

    Update settings.py

--- aoewebapp/settings.py
+++ aoewebapp/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://aoewebapp.alan.com.ar/']
+CSRF_TRUSTED_ORIGINS = ['https://aoewebapp-production.up.railway.app/']
 
 
 # Application definition
