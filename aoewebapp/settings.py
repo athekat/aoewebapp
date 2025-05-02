56c2cd9aa7419c7e085cb62c3a8abe5d9c3d1d83
Alan <63990513+athekat@users.noreply.github.com>
2025-05-02 18:32:42 -0300

    Update settings.py

--- aoewebapp/settings.py
+++ aoewebapp/settings.py
@@ -25,10 +25,15 @@ SECRET_KEY = 'django-insecure-!q%+0g_ke_!1#!$9*yb1f)&5q4em#@z-x89_)^)1iow9v))cia
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'your-subdomain.up.railway.app',
+    'aoewebapp.alan.com.ar',
+]
 
-CSRF_TRUSTED_ORIGINS = ['https://aoewebapp-production.up.railway.app/']
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://your-subdomain.up.railway.app',
+    'https://aoewebapp.alan.com.ar'
+]
 
 # Application definition
 
